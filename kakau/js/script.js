db68fc2eec9b089f19d76ffa7fdf18c4e2da6399
Gabriel de Castro <82734478+Nightfall117@users.noreply.github.com>
2023-11-16 11:51:40 -0300

    Add files via upload

---
+++ kakau/js/script.js
@@ -0,0 +1,25 @@
+//Este código mostrar um elemento se ele estiver na viewport
+//Esta função tem como base o método "observe". O método observe() em JavaScript é utilizado para observar alterações em um elemento alvo ou em uma coleção de elementos através do uso da API Intersection Observer. O Intersection Observer é uma API que permite ao código ser notificado quando um elemento entra ou sai da área de interseção com o viewport do navegador.
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.querySelector('.midia').play();
+            entry.target.classList.add('show');
+
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+});
+//aqui os elementos são guardados na variável hiddeeElements e cada elemento da lista tem seu próprio observer
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+/*vai tomar no cu piranha*/
+ function appear(){
+     document.body.style.overflowY = 'scroll';
+ }
+
+ let but= document.getElementById('but');
+ but.addEventListener('click',appear()); 
+ 
