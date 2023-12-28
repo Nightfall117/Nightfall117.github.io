f49de65ae020f0e77a86230355bb6fb414f1b368
Gabriel de Castro <82734478+Nightfall117@users.noreply.github.com>
2023-12-27 23:36:55 -0300

    code update
    in this update I added auto pause for the midia.

--- kakau/js/script.js
+++ kakau/js/script.js
@@ -9,6 +9,7 @@ const observer = new IntersectionObserver((entries) => {
 
         } else {
             entry.target.classList.remove('show');
+            entry.target.querySelector('.midia').pause();
         }
     })
 });
